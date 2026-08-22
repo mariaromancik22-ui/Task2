8576b1b1dd69b63bf32439186caf448476cfa627
maria_romanchik <mariaromancik22@gmail.com>
2026-08-22 21:40:50 +0300

    score highscore

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -15,6 +15,10 @@ public class GameManager : MonoBehaviour
 
     public TextMeshProUGUI game_over_text;
     public Button retry_button;
+
+    public TextMeshProUGUI score_text;
+    public TextMeshProUGUI high_score_text;
+    private float score;
     private void Awake()
     {
         if (Instance == null)
@@ -42,11 +46,13 @@ public class GameManager : MonoBehaviour
         {
             Destroy(obstacle.gameObject);
         }
+        score = 0f;
         game_speed = initial_game_speed;
         enabled = true;
         spawner.gameObject.SetActive(true);
         game_over_text.gameObject.SetActive(false);
         retry_button.gameObject.SetActive(false);
+        update_high_score();
     }
     public void GameOver()
     {
@@ -55,9 +61,25 @@ public class GameManager : MonoBehaviour
         spawner.gameObject.SetActive(false);
         game_over_text.gameObject.SetActive(true);
         retry_button.gameObject.SetActive(true);
+        update_high_score();
     }
     private void Update()
     {
         game_speed += game_speed_increase * Time.deltaTime;
+        score += game_speed * Time.deltaTime;
+        score_text.text = Mathf.FloorToInt(score).ToString("D5");
+    }
+
+    private void update_high_score()
+    {
+        float high_score = PlayerPrefs.GetFloat("high_score", 0);
+
+        if (score > high_score)
+        {
+            high_score = score;
+            PlayerPrefs.SetFloat("high_score", high_score);
+            PlayerPrefs.Save();
+        }
+        high_score_text.text = Mathf.FloorToInt(high_score).ToString("D5");
     }
 }
