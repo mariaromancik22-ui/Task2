a94792c23a6d1b03ade07847c379848833301655
maria_romanchik <mariaromancik22@gmail.com>
2026-08-25 18:50:55 +0300

    task 5 no particles because of bug

--- Assets/Scripts/Ground.cs
+++ Assets/Scripts/Ground.cs
@@ -1,16 +1,16 @@
 using UnityEngine;
-
+[RequireComponent(typeof(MeshRenderer))]
 public class Ground : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private MeshRenderer mesh_renderer;
-    void Awake()
+    private void Awake()
     {
         mesh_renderer = GetComponent<MeshRenderer>();
     }
     //раньше скорость - была константой, которую подбирали вручную для земли и препятсвтвий. теперь используем gamemanager, чтобы всё было соеденино
  
-    void Update()
+    private void Update()
     {
         float speed = GameManager.Instance.game_speed / transform.localScale.x;
         mesh_renderer.material.mainTextureOffset += Vector2.right * Time.deltaTime * speed;
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -13,6 +13,9 @@ public class Player : MonoBehaviour
     private Animator animator;
 
     public bool dead = false; //используется в менеджере для перезапуска игры
+
+    public GameObject dust_prefab; // ← Перетащи сюда префаб Dust
+    private bool landed = false; // ← Чтобы не спамило
     void Awake()
     {
         character = GetComponent<CharacterController>();
@@ -28,15 +31,24 @@ public class Player : MonoBehaviour
     {
         if (dead)
         {
-           
             return;
         }
-        direction += Vector3.down * gravity * Time.deltaTime;
+       
+        direction +=  gravity * Time.deltaTime * Vector3.down;
         animator.SetBool("Roar", false); //не рычит
+        animator.SetBool("Dead", false);
+
 
         if (character.isGrounded)
         {
+            Debug.Log("На земле");
+            if (landed)
+            {
+                SpawnDust();
+            }
+
             direction = Vector3.down;
+            animator.SetBool("Run", true);
             animator.SetBool("Jump", false);//на земле не прыгаем
             animator.SetBool("Flip", false); //не делает сальто
 
@@ -48,13 +60,19 @@ public class Player : MonoBehaviour
 
             if (Input.GetButtonDown("Jump") && jump_count == 0)
             {
-                animator.SetBool("Jump", true); //нажали пробел - прыгаем
+               
+                animator.SetBool("Jump", true);
+                animator.SetBool("Run", false);  //нажали пробел - прыгаем
                 direction = Vector3.up * jumpforce;
                 jump_count = 1;
+                Debug.Log("Прыжок!");
+
             }
         }
         else
         {
+            Debug.Log("В воздухе!");
+            animator.SetBool("Run", false);
             if (Input.GetButtonDown("Jump") && jump_count < 2)
             {
                 direction = Vector3.up * jumpforce;
@@ -65,6 +83,7 @@ public class Player : MonoBehaviour
                 {
                     animator.SetBool("Flip", true); //сальто
                 }
+               
             }
         }
 
@@ -76,14 +95,28 @@ public class Player : MonoBehaviour
             animator.SetBool("Roar", true);
         }
     }
+    private void SpawnDust()
+    {
+        if (dust_prefab != null)
+        {
+            GameObject dust = Instantiate(dust_prefab);
+            Destroy(dust, 1f);
+        }
+    }
     //столкновение
     //вместо остановки времени - сделали gameover и рестарт в менеджере
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Obstacle"))
         {
-            dead = true;
+            animator.SetBool("Run", false);
+            animator.SetBool("Jump", false);
+            animator.SetBool("Flip", false);
+            animator.SetBool("Roar", false);
+
+            // 
             animator.SetBool("Dead", true);
+            dead = true;
             GameManager.Instance.GameOver();
         }
 
